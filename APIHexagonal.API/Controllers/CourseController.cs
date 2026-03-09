a87b72da8059a3c52f9b8f9e6f1192c8f8403565
Lucas Gabriel <lucaas.gabriel1804@gmail.com>
2026-03-09 16:28:50 -0300

    desenvolvendo a camada API com as controllers e a program

---
+++ APIHexagonal.API/Controllers/CourseController.cs
@@ -0,0 +1,65 @@
+﻿using APIHexagonal.Application.DTOs;
+using APIHexagonal.Application.Interfaces.IServices;
+using APIHexagonal.Domain.Interfaces.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIHexagonal.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController: ControllerBase
+    {
+        protected readonly ICourseService _courseService;
+        public CourseController(ICourseService service)
+        {
+            _courseService = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var courses = _courseService.GetAll();
+            return Ok(courses);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var course = _courseService.GetById(id);
+
+            if (course == null) 
+                return NotFound();
+
+            return Ok(course);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateCourseDTO course)
+        {
+            _courseService.Create(course);
+            return Created();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, CreateCourseDTO course)
+        {
+            var result = _courseService.Update(id, course);
+
+            if (!result) 
+                return NotFound();
+
+            return Ok("Course updated successfully.");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var result = _courseService.Delete(id);
+
+            if (!result) 
+                return NotFound();
+
+            return Ok("Course deleted successfully.");
+        }
+    }
+}
---
+++ APIHexagonal.API/Controllers/StudentController.cs
@@ -0,0 +1,78 @@
+﻿using APIHexagonal.Application.DTOs;
+using APIHexagonal.Application.Interfaces.IServices;
+using APIHexagonal.Application.Services;
+using APIHexagonal.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIHexagonal.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        protected readonly IStudentService _studentService;
+        public StudentController(IStudentService service)
+        {
+            _studentService = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var students = _studentService.GetAll();
+            return Ok(students);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var student = _studentService.GetById(id);
+
+            if (student == null) 
+                return NotFound();
+
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateStudentDTO student)
+        {
+            _studentService.Create(student);
+            return Created();
+        }
+
+        [HttpPost("enroll")]
+        public IActionResult Enroll([FromBody] CreateEnrollDTO enroll)
+        {
+            var result = _studentService.Enroll(enroll.IdStudent, enroll.IdCourse);
+
+            if (!result) 
+                return BadRequest("Enrollment failed.");
+
+            return Ok("Enrollment successful.");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, CreateStudentDTO student)
+        {
+            var result = _studentService.Update(id, student);
+
+            if (!result) 
+                return NotFound();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var result = _studentService.Delete(id);
+
+            if (!result) 
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
